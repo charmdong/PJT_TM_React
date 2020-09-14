476b2eadacc4a44128adef70b3772f77773fb96d
Donggun.Chung <chungdk1117@gmail.com>
2020-09-14 22:38:50 +0900

    [React Props, state]

--- src/App.js
+++ src/App.js
@@ -1,16 +1,11 @@
 import React, { Component, Fragment } from "react";
-import logo from "./logo.svg";
 import "./App.css";
+import MyComponent from "./MyComponent";
+import Counter from "./Counter";
+import Say from "./Say";
 
 const App = () => {
-    return (
-        <div align="center">
-            <h1>Hello world</h1>
-            <button>LOGIN</button>
-            <button>MY PAGE</button>
-            <button>BOARD LIST</button>
-        </div>
-    );
+    return <Say/>;
 };
 
 export default App;
