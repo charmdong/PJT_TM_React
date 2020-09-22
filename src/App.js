a21651e41049323d7c9d0006bbd884d100ae4773
Donggun.Chung <chungdk1117@gmail.com>
2020-09-22 22:24:01 +0900

    React : Component Iteration

--- src/App.js
+++ src/App.js
@@ -2,14 +2,13 @@ import React, { Component } from 'react';
 import ValidationSample from './ValidationSample';
 import RefSample from './RefSample';
 import ScrollBox from './ScrollBox'
+import IterationSample from './IterationSample';
 import './App.css';
 
 class App extends Component {
   render() {
     return (
-      <div>
-        <ScrollBox/>
-      </div>
+      <IterationSample/>
     );
   }
 }
